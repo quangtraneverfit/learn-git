c6f4c91a064ab177f35ee0249f953ce29feaef33
quang tung <quangtung@quangs-MacBook-Pro-2.local>
2024-03-03 21:35:01 +0700

    tungtran:lg-003 update source code

--- student.cpp
+++ student.cpp
@@ -1,5 +1,7 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"le dai ca"<<endl;
+int main()
+{
+    cout << "le dai ca" << endl;
+    print("hahha")
 }
--- teacher.cpp
+++ teacher.cpp
@@ -1,3 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-cout<<"cho tung ni"<<endl;
+cout << "cho tung ni" << endl;
+cout << "oc bo le" << endl;
