db7b9967d8cc99c8509e2912a626249d12a34d3f
quang tung <quangtung@quangs-MacBook-Pro-2.local>
2024-03-03 20:30:45 +0700

    tungtan:lg-001 init repo

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int inputArr[6] = {4, 6, 3, 2, 8, 1};
+  int outputArr[6];
+  int a = 5;
+  int *b = &a;
+  int inputArrLength = sizeof(inputArr) / sizeof(inputArr[0]);
+  int newEle;
+  for (int i = 0; i < inputArrLength; i++)
+  {
+    newEle = -1;
+    for (int j = i; j < inputArrLength; j++)
+    {
+      if (inputArr[i] < inputArr[j])
+      {
+        newEle = inputArr[j];
+        break;
+      }
+    }
+    outputArr[i] = newEle;
+  }
+
+  // for (int k = 0; k < inputArrLength; k++)
+  // {
+  //   cout << outputArr[k] << " ,";
+  // }
+  return 0;
+}
