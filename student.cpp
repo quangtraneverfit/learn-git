b686d07c6ed6bcdc3436f1af709728161aa9839e
quang tung <quangtung@quangs-MacBook-Pro-2.local>
2024-03-18 16:43:12 +0700

    tungtran:lg-017 test cherry pick
    tungtran:lg-017 test cherry pick
    
    tungtran:lg-017 test cherry pick

--- student.cpp
+++ student.cpp
@@ -3,8 +3,9 @@ using namespace std;
 int main()
 {
     cout << "le dai ca ma con ngu" << endl;
-    print("hahha hihi")
+    print("hahha hihi");
     cout << "con ga le le" << endl;
     cout << "toi la le" << endl;
-    print("hahha12")
+    print("hahha12");
+    print("hahha12");
 }
