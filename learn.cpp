2a71e0261db9604f7dd247918f27e302fd76c05a
quangtung19999 <katonnapro@gmail.com>
2024-04-15 10:12:51 +0700

    lenguyen:lg-01 update learn.cpp
    lenguyen:lg-005 all
    
    lenguyen:lg-01 update learn.cpp
    
    lenguyen:lg-005 all
    
    Update learn.cpp
    
    lenguyen:lg-006 add teacher1.cpp

--- learn.cpp
+++ learn.cpp
@@ -3,6 +3,12 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     cout<<"hello1";
+<<<<<<< HEAD
     cout<<"ok";
+    cout<<"oki la";
+=======
+    cout<<"ok con de
+        ";
+>>>>>>> 008610b (Update learn.cpp)
     return 0;
 }
--- newday.cpp
+++ newday.cpp
@@ -1,7 +1,3 @@
 #include<iostream>
 using namespace std;
-<<<<<<< HEAD
-cout<<"ok no5"
-=======
 cout<<"ok no 1"
->>>>>>> b283e5d0c46555530f4abd2747a0f764ccfd9430
